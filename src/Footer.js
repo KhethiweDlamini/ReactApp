1b86d23ae65f895696ae23c19d8621b1c99ed072
Khethiwe R.L. Dlamini <62747894+KhethiweDlamini@users.noreply.github.com>
2024-03-11 20:50:51 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,13 +6,18 @@ export default function Footer() {
     <footer>
       <p>
         This project was coded by{" "}
-        <a href="https://github.com/KhethiweDlamini" target="_blank">
+        <a
+          href="https://github.com/KhethiweDlamini"
+          target="_blank"
+          rel="noreferrer"
+        >
           K.R.L. Dlamini
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/KhethiweDlamini/weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           on GitHub
         </a>{" "}
@@ -20,6 +25,7 @@ export default function Footer() {
         <a
           href="https://app.netlify.com/sites/my-weather-app-krld77/overview"
           target="_blank"
+          rel="noreferrer"
         >
           hosted on Netlify
         </a>
